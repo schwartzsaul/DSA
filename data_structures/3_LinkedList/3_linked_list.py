788de0f895da22b02f6738bd0d6cc9ce6c96620b
Saul Schwartz <ssch4281@uni.sydney.edu.au>
2023-06-17 13:53:34 +1000

    solved singly linked lists

--- data_structures/3_LinkedList/3_linked_list.py
+++ data_structures/3_LinkedList/3_linked_list.py
@@ -14,7 +14,7 @@ class LinkedList:
         itr = self.head
         llstr = ''
         while itr:
-            llstr += str(itr.data)+' --> ' if itr.next else str(itr.data)
+            llstr += str(itr.data) + ' --> ' #if itr.next else str(itr.data)
             itr = itr.next
         print(llstr)
 
@@ -84,16 +84,44 @@ class LinkedList:
         self.head = None
         for data in data_list:
             self.insert_at_end(data)
+    
+    def insert_after_value(self, data_after, data_to_insert):
+        # Search for first occurance of data_after value in linked list
+        itr = self.head
+        while itr:
+            if itr.data == data_after:
+                # Now insert data_to_insert after data_after node
+                itr.next = Node(data_to_insert, itr.next)
+                break
+            itr = itr.next
+    
+    def remove_by_value(self, data):
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+        
+        itr = self.head
+        while itr.next:
+            if itr.next.data == data:
+                itr.next = itr.next.next
+                break
+            itr = itr.next
 
+        
 
 if __name__ == '__main__':
     ll = LinkedList()
-    ll.insert_values(["banana","mango","grapes","orange"])
-    ll.insert_at(1,"blueberry")
-    ll.remove_at(2)
+    ll.insert_values(["orange","banana","grapes","mango"])
     ll.print()
-
-    ll.insert_values([45,7,12,567,99])
-    ll.insert_at_end(67)
+    ll.insert_after_value("mango","apple") # insert apple after mango
+    ll.print()
+    ll.remove_by_value("orange") # remove orange from linked list
+    ll.print()
+    ll.remove_by_value("figs")
+    ll.print()
+    ll.remove_by_value("banana")
+    ll.remove_by_value("mango")
+    ll.remove_by_value("apple")
+    ll.remove_by_value("grapes")
     ll.print()
 
